46c89d84a6c5d91ab27ff759c9e64f953980348b
xqyjlj <xqyjlj@126.com>
2022-12-16 17:50:09 +0800

    ✨ feat(gitee.py): add gitee api

---
+++ gitee.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import urllib
+import urllib.request
+import json
+
+token = os.environ["GITEE_TOKEN"]
+owner = os.environ["GITEE_OWNER"]
+
+
+def create_repository(repo):
+    url = f'https://gitee.com/api/v5/orgs/{owner}/repos'  # organization
+    data = {}
+    data["access_token"] = token
+    data["name"] = repo
+    data["has_issues"] = True
+    data["has_wiki"] = True
+    data["can_comment"] = True
+    data = json.dumps(data)
+    data = data.encode('utf-8')
+    request = urllib.request.Request(url, data, {'content-type': 'application/json', 'charset': 'charset'})
+    try:
+        response = urllib.request.urlopen(request)
+        resp = response.read().decode('utf-8')
+    except Exception as e:
+        print('create repositories: ' + repo + ' failed')
+        print('e.code  : ' + str(e.code))
+        print('e.reason: ' + str(e.reason))
+    return json.loads(resp)
+
+
+def get_or_create_repository(repo):
+    url = f'https://gitee.com/api/v5/repos/{owner}/{repo}?access_token={token}'
+    request = urllib.request.Request(url)
+    try:
+        response = urllib.request.urlopen(request)
+        resp = response.read().decode('utf-8')
+    except Exception as e:
+        if e.code == 404 or e.code == 410:
+            return create_repository(repo)
+        else:
+            print('get repository: ' + repo + ' failed, and http code not 404 ot 410!')
+            print('e.code  : ' + str(e.code))
+            print('e.reason: ' + str(e.reason))
+    return json.loads(resp)
+
+
+if __name__ == "__main__":
+    resp = get_or_create_repository("csp")
+    print(resp)
